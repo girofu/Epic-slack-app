8045d4cc2e8980f492f720ea92a6366ed55bac1b
CWF <girofu@gmail.com>
2022-12-12 23:58:57 +0800

    fix the slack bot token

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { SLACK_BOT_TOKEN, SLACK_APP_TOKEN, SLACK_SIGNING_SECRET } from "./constants";
+import { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET } from "./constants";
 
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
