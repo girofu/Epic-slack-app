92070888dda0c32d95860912f863ae3608955683
CWF <girofu@gmail.com>
2023-05-15 22:57:57 +0800

    reply and editted post debug

--- src/index002.js
+++ src/index002.js
@@ -4,8 +4,10 @@ require("dotenv").config()
 
 // const app = require("./homePage.js")
 const api = require("./api.js")
+const reply = require("./reply.js")
 
 module.exports = {
   // app: app,
   api: api,
+  reply: reply
 };
